09abd8c2f2cab68e92a98e02d2ea1040ce404c35
devil-cyber <52333306+devil-cyber@users.noreply.github.com>
2019-12-15 14:35:49 +0530

    Add files via upload
    This is Neural network Program

---
+++ NeuralNetworkModal.m
@@ -0,0 +1,18 @@
+function NeuralNetworkModal()
+  load('ex3data1.mat');
+  m=size(X,1);
+  input_node=400;
+  hidden_node=25;
+  num_labels=10;
+  load('ex3weights.mat');
+  rp = randperm(m);
+
+for i = 1:m
+    % Display 
+    fprintf('\nDisplaying Example Image\n');
+    displayData(X(rp(i), :));
+
+    pred = predict(Theta1, Theta2, X(rp(i),:));
+    fprintf('\nNeural Network Prediction: %d (digit %d)\n', pred, mod(pred, 10));
+    end
+endfunction
---
+++ NeuralNetworkPredict.m
@@ -0,0 +1,11 @@
+function p=NeuralNetworkPredict(theta1,theta2,X)
+  m = size(X, 1);
+  a1=[ones(m,1) X];
+z2=a1*Theta1';
+a2=sigmoid(z2);
+a2=[ones(size(a2,1),1) a2];
+z3=a2*Theta2';
+a3=sigmoid(z3);
+[prob,p]=max(a3,[],2);
+  
+endfunction
